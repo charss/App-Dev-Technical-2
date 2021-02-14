27129512909ae90513e9aacaee32a73cd12e95e8
Charles Reyes <charleskylereyes16@gmail.com>
2021-02-14 20:57:41 +0800

    Initialized files

---
+++ Prob3/index.php
@@ -0,0 +1,38 @@
+<!-- Create a formula for each volume of shapes (Cube, Right Rectangular Prism, Prism or Cylinder, Pyramid or Cone and Sphere) then display the result. Please see the sample below: -->
+
+<html>
+	<head>
+	</head>
+	<body>
+		<?php
+
+			function cube($side) {
+				return $side ^ 3;
+			}
+
+			function right_rect_prism($width, $height, $length) {
+				return $width * $height * $length;
+			}
+
+			function prism($base, $height) {
+				return $base * $height;
+			}
+
+			function cylinder() {
+				return 
+			}
+
+			function pyramid() {
+
+			}
+
+			function cone() {
+
+			}
+
+			function sphere() {
+
+			}
+		?>
+	</body>
+</html>
