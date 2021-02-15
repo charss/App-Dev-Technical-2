abe890fa02a8d13af582e8dccfeed159536f0c05
Charles Reyes <charleskylereyes16@gmail.com>
2021-02-16 00:31:03 +0800

    Initialized files

---
+++ Prob2/index.php
@@ -0,0 +1,9 @@
+<!-- Using ARRAYS, create a fruit directory that will show the image, name, description and facts about the fruit (create at least 10 fruits sort in alphabetical order). Implement a HTML and CSS code for the structure and design of the webpage. Please see the sample output below. -->
+
+<html>
+	<head>
+	</head>
+	<link rel='stylesheet' href='style.css'>
+	<body>
+	</body>
+</html>
