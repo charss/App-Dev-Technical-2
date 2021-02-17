632828f5e4eba8c8d9fbb5e25390d992e001db17
Charles Reyes <charleskylereyes16@gmail.com>
2021-02-17 17:59:27 +0800

    Finished Program

--- Prob2/index.php
+++ Prob2/index.php
@@ -1,7 +1,3 @@
-<!-- Using ARRAYS, create a fruit directory that will show the image, name, description and facts about the fruit (create at least 10 fruits sort in alphabetical order). Implement a HTML and CSS code for the structure and design of the webpage. Please see the sample output below.
-
-https://www.healthline.com/nutrition/20-healthiest-fruits#TOC_TITLE_HDR_2 -->
-
 <html>
 	<head>
 		<title></title>
@@ -13,67 +9,55 @@ https://www.healthline.com/nutrition/20-healthiest-fruits#TOC_TITLE_HDR_2 -->
 			    return $a['name'] <=> $b['name'];
 			}
 
-			function sortByAscName($a, $b) {
-			    return $b['name'] <=> $a['name'];
-			}
-
-			function sortByYoung($a, $b) {
-			    return $a['age'] <=> $b['age'];
-			}
-
-			function sortByOld($a, $b) {
-			    return $b['age'] <=> $a['age'];
-			}
-
-
 			// names, images, description, facts
 			$fruits = array(
 				array('name'   => 'Grapefruit', 
 					  'img_id' => 'images/grapefruit.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Yellow (outside)<br>Red (inside)</p>", 
 					  'fact'    => "<p>One of the healthiest citrus fruits.</p> <p>Besides being a good source of vitamins and minearls, it is known for its ability to aid weight loss and reduce insulin resistance.</p>"),
 				array('name'   => 'Pineapple', 
 					  'img_id' => 'images/pineapple.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Yellow</p>", 
 					  'fact'    => "<p>Among the tropical fruits, pineapple is a nutritious superstar.</p> <p>Pineapple is rich in vitamic C and manganeses. Its bromelain content may fight inflammation and reduce the risk of cancer.</p>"),
 				array('name'   => 'Avocado', 
 					  'img_id' => 'images/avocado.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Deep Purple</p>", 
 					  'fact'    => "<p>Most fruits are high in carbs, while avocado is low in carbs and comprised mainly of healthy fats.</p> <p>Avocados are rich in healthy fats and potassium, both of which are well known for their role in promoting heart health.</p>"),
 				array('name'   => 'Blueberries', 
 					  'img_id' => 'images/blueberries.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color</p>Deep Purple</p>", 
 					  'fact'    => "<p>The antioxidants in blueberries may reduce the risk of chronic conditions, such as heart disease, diabetes and Alzheimer’s.</p> <p>Blueberries are rich in a few important nutrients. They have a high antioxidant capacity and immune-enhancing properties, which may protect the body from illness.</p>"),
 				array('name'   => 'Apples', 
 					  'img_id' => 'images/apples.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Red</p>", 
 					  'fact'    => "<p>Apples are among the most popular fruits, and also happen to be incredibly nutritious.</p> <p>Apples are very nutritious. Their nutrients, antioxidants and fiber may reduce the risk of disease and improve digestion.</p>"),
 				array('name'   => 'Pomegranate', 
 					  'img_id' => 'images/pomegranate.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Red</p>", 
 					  'fact'    => "<p>Pomegranates are among the healthiest fruits you can eat.</p> <p>Pomegranates have wide-ranging health benefits. They are incredibly high in antioxidants and other plant compounds that can help reduce inflammation and prevent disease.</p>"),
 				array('name'   => 'Mango', 
 					  'img_id' => 'images/mango.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Yellow</p>", 
 					  'fact'    => "<p>Mangoes are an excellent source of vitamin C.</p> <p>Mangoes contain vitamin C and soluble fiber. They also contain plant compounds with antioxidant and anti-inflammatory effects.</p>"),
 				array('name'   => 'Strawberries', 
 					  'img_id' => 'images/strawberries.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Red</p>", 
 					  'fact'    => "<p>Strawberries are highly nutritious.</p> <p>Strawberries are rich in several nutrients and antioxidants. Eating them may help control your blood sugar levels and reduce your risk of some diseases.</p>"),
 				array('name'   => 'Cranberries', 
 					  'img_id' => 'images/cranberries.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Red</p>", 
 					  'fact'    => "<p>Cranberries have impressive health benefits.</p> <p>Cranberries are rich in several nutrients and antioxidants. They also contain beneficial plant compounds that help prevent urinary tract infections.</p>"),
 				array('name'   => 'Lemons', 
 					  'img_id' => 'images/lemons.jpg', 
-					  'desc'   => "<p></p>", 
+					  'desc'   => "<p>Color<br>Yellow</p>", 
 					  'fact'    => "<p>Lemons are a very healthy citrus fruit known for their high vitamin C content.</p> <p>Lemons are rich in vitamin C and other plant compounds that may promote heart health, boost weight loss and help prevent kidney stones.</p>"),
-
-
 			);
-		?>
 
-		<table>
+			usort($fruits, 'sortByDescName');
+		?>
+	
+		<h1>My Favorite Fruits</h1>
+		<table class='middle'>
 			<tr>
 				<th></th>
 				<th></th>
